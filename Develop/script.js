c56099010eb1aea469908ae210a58c855ee4fcba
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2022-11-30 16:10:50 -0500

    commit

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
@@ -15,42 +14,22 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
-  var possibleSpecial = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-  var letters = "abcdefghijklmnopqrstuvwxzy";
+  var possibleSpecial = ['!#$%&()*+,-./:;<=>?@[\]^_`{|}~"];
+  var letters = ['a', 'b', 'c', 'd', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var numbers = "0987654321";
-
 
   var isUppercase = confirm('Would you like to include uppercase letters?');
   var isLowercase = confirm('Would you like to include lowercase letters?');
   var isSpecial = confirm('Would you like to include special characters?');
   var isNumber = confirm('Would you like to include numbers?');
-  var charCount = confirm('How many characters would you like to use? (8-128')
-  
-  if (isUppercase) {
-    // USE UPPERCASE
-    console.log('uppercase')
+  var charCount = prompt('How many characters would you like to use? (8-128)');
+    
+  if (charCount < 8 || charCount > 128) {
+      alert('Too many/Too few characters.');
+    };
+    console.log ('character number out of range')
 
-    if (isLowercase) {
-      // USE LOWERCASE
-      console.log('lowercase')
-    }
-
-    if (isSpecial) {
-      // USE SPECIAL
-      console.log('special')
-    }
-
-    if (isNumber) {
-      // USE NUMBERS
-      console.log('number')
-    }
-  }
-
-
-
-
-
-
+    
 }
 
 
