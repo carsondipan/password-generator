4fcfdfa28abeb28daa8fbd2f305194ec1683a6c1
Carson DiPangrazio <carson.dipangrazio@gmail.com>
2022-11-30 17:17:44 -0500

    confirmation success I think

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,10 @@
-var possibleSpecial = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '/', '>', '<', '+', '=', ']', '[',];
-var letters = ['a', 'b', 'c', 'd', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '/', '>', '<', '+', '=', ']', '[',];
+var lowercase = ['a', 'b', 'c', 'd', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
 
 var charCount = 8;
+var choices = [];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -12,33 +14,31 @@ generateBtn.addEventListener("click", askPrompts);
 
 
 function askPrompts(){ //Asks the user a series of prompts, stores their choices
-  isNumber = confirm('Would you like to include numbers?');
-    if(confirm('Do you want numbers in your password?'));
-  
-    isUppercase = confirm('Would you like to include uppercase letters?');
-  
-  isLowercase = confirm('Would you like to include lowercase letters?');
-  
-  isSpecial = confirm('Would you like to include special characters?');
-  
-  charCount = prompt("How many characters would you like to use? (8-128)");
-    
+   charCount = prompt("How many characters would you like to use? (8-128)");
     if(isNaN(charCount) || charCount < 8 || charCount > 128) {
       alert('Length must be a number and between 8 and 128. Try again.')
       return false;
-    }
-
-
+    } 
+  
+    if(confirm('Would you like to include numbers?'));
+      choices = choices.concat(numbers);
+  
+    if(confirm('Would you like to include uppercase letters'));
+      choices = choices.concat(uppercase);
+  
+    if(confirm('Would you like to include lowercase letters?'));
+      choices = choices.concat(lowercase);
+    if(confirm('Would you like to include special characters?'));
+      choices = choices.concat(special)
+  
 }
   
 function generatePassword() {
   //generatePassword based on prompt responses 
-  if (charCount < 8 || charCount > 128) {
-      alert('Too many/Too few characters.');
-    };
-    console.log ('character number out of range')
-
-    
+  var password = "";
+  for(var i = 0; i < charCount; i++) {
+           
+  }    
 }
 
 function writePassword() {
